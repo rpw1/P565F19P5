b1b81af25d5400b6131de69a86e73349fd843a71
dronske <dronske@iu.edu>
2021-02-17 21:27:06 -0500

    initializing app and log in system

---
+++ website/auth.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, request
+
+auth = Blueprint("auth", __name__)
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method = "POST":
+        email = request.form.get("email")
+        fname = request.form.get("fname")
+        lname = request.form.get("lname")
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+        if password != confirm:
+            #finish
+        else:
+            #add
+    return render_template("register.html")
