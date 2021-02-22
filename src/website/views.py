cd6bb31f7585805ac35376bedd9e60e94527fc4a
dronske <dronske@iu.edu>
2021-02-22 17:13:02 -0500

    logout button on navbar and username on homepage

--- src/website/views.py
+++ src/website/views.py
@@ -1,12 +1,12 @@
 from flask import Flask, Blueprint, redirect, url_for, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 @login_required
 def home():
-    return render_template("index.html")
+    return render_template("index.html", user=current_user)
 
 if __name__ == "__main__":
     app.run(debug=True)
