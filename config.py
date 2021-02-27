6d4b271e00fd06cd0a15bfc4064bf8df6272488d
rpw1 <ryan.will@outlook.com>
2021-02-26 19:39:47 -0500

    heroku stuff

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = '3154d28d-afb0-46e5-b219-9c1c82c884b4'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
