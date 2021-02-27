18111638c710a2d0e67e0b5c6e8cb4626f86a8a5
rpw1 <ryan.will@outlook.com>
2021-02-26 20:38:00 -0500

    added environment variables

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,14 @@
 from website import create_app
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 app = create_app()
-app.config.from_object(os.environ['APP_SETTINGS'])
-print(os.environ['APP_SETTINGS'])
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+app.config.from_object(os.environ.get("APP_SETTINGS"))
+print(os.environ.get("APP_SETTINGS"))
 if __name__ == "__main__":
     app.run(debug=True, ssl_context="adhoc")
