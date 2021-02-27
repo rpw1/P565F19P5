fec17a40786cf7ba5b7a46d6dec2cc3accd86f72
rpw1 <ryan.will@outlook.com>
2021-02-26 19:39:20 -0500

    heroku

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,8 @@
 from website import create_app
+import os
 
 app = create_app()
-
-print("Add and argument after the file to specify which database you are using")
-print("Don't an argument for production")
-print("1 - Ryan's Database")
-print("2 - Derek's Database")
-print("3 - Ben's Database")
-print("4 - Izzy's Database")
-
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 if __name__ == "__main__":
     app.run(debug=True, ssl_context="adhoc")
