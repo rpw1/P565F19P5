0f026e62475b60cb4c0498dfeb1c2f37cbaf3dea
Izabel Ruiz <izruiz@iu.edu>
2021-03-16 12:20:26 -0400

    more progress tracking

---
+++ src/database/progress_tracking_database.py
@@ -0,0 +1,19 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+from decouple import config
+
+class ProgressTrackingDatabase:
+
+    def __init__(self, dynamodb = None):
+        self.dynamodb = dynamodb
+        self.user_table = None
+
+    def check_database(self):
+        if not self.dynamodb:
+            self.dynamodb = boto3.resource(
+                'dynamodb', 
+                aws_access_key_id = config('AWS_ACCESS_KEY_ID'), 
+                aws_secret_access_key = config('AWS_SECRET_ACCESS_KEY'),
+                region_name = config('AWS_REGION')
+                )
+        self.content_table = self.dynamodb.Table('progress_tracking')
