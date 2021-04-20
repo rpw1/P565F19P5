d4008a24e2a7a6fad0d4b80f3ca3afb9a5af24d3
rpw1 <ryan.will@outlook.com>
2021-04-20 18:22:17 -0400

    changing pipfile/piplock bc stack overflow told me so

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from src.website import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, ssl_context="adhoc")
+    app.run(debug=True, ssl_context="adhoc", port=33507)
